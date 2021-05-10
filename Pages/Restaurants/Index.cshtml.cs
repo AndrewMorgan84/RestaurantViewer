815f5d887cd5362c02189140f5a24f9f300a7292
Andrew <andrew.morgan@e.org>
2021-05-10 16:15:20 +0100

    Added InMemory Data Added Index page data

--- Pages/Restaurants/Index.cshtml.cs
+++ Pages/Restaurants/Index.cshtml.cs
@@ -4,18 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestaurantViewer.Core;
+using RestaurantViewer.Data;
 
 namespace RestaurantViewer.Pages.Restaurants
 {
     public class IndexModel : PageModel
     {
-        public IndexModel()
-        {
+        private readonly IRestaurantData restaurantData;
 
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public IndexModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
         }
 
         public void OnGet()
         {
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RestaurantViewer.Data;
 
 namespace RestaurantViewer
 {
@@ -24,6 +20,7 @@ namespace RestaurantViewer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IRestaurantData,InMemoryRestaurantData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
